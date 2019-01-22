3f0e22cb249ebded4bbe1d705bfc5f62a3a93650
shabnum <shabnum@LAPTOP-ORBPMUQU>
2019-01-22 15:03:11 +0530

    first push

---
+++ Mavenproject1/src/main/java/Logout.java
@@ -0,0 +1,19 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Logout {
+
+	WebDriver driver;
+	By Logout=By.linkText("Log Out");
+	public Logout(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+    public void clickOnLogOut()
+    {
+    	driver.findElement(Logout).click();
+    }
+}
+
